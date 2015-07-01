61be7540138ad1bd53ad7cd2418ceef9a3ba16c4
catalin-enache <catalinenache78@gmail.com>
2015-07-01 15:24:11 +0300

    updated java_sketch_example

--- java_sketch_example/src/net/netiko/MyTest.java
+++ java_sketch_example/src/net/netiko/MyTest.java
@@ -1,29 +1,74 @@
 package net.netiko;
 
 import processing.core.*;
-import netiko.stage.Test;
+
+
+import netiko.stage.*;
+
 
 public class MyTest extends PApplet {
 
-    Test t;
+    int w = 600;
+    int h = 600;
+    boolean isCartesian = true;
+
+    PointDraggable p1;
 
     public static void main(String args[]) {
         // full-screen mode can be activated via parameters to PApplets main method.
-        System.out.println("in mains");
         PApplet.main(new String[]{"net.netiko.MyTest"});
     }
 
     public void setup() {
-        size(400,400);
-        background(0);
-        t = new Test(this);
+        Stage.startSetup(this, w, h, isCartesian);
+        p1 = Stage.pointDraggable(0, 0, 10);
+
+        reactToEvents();
+
+        Stage.endSetup();
     }
 
     public void draw() {
-        stroke(255);
-        if (mousePressed) {
-            //line(mouseX,mouseY,pmouseX,pmouseY);
-            t.print();
-        }
+        Stage.startDraw();
+
+        Stage.endDraw();
+    }
+
+    private void reactToEvents () {
+        Stage.addEventClient(new IStageEventClient(){
+            public Event.Name[] registerForEvents() {
+                // mousePressed, mouseReleased, mouseMove, draggableDragged, pointUpdated, shapeModified, draggableSelected
+                return new Event.Name[]{
+                        Event.Name.mousePressed,
+                        Event.Name.mouseReleased,
+                        Event.Name.mouseMove,
+                        Event.Name.pointUpdated,
+                        Event.Name.draggableSelected,
+                        Event.Name.draggableDragged,
+                        Event.Name.shapeModified
+                };
+            }
+            public void onEvent(Event evt, Object emitter) {
+                if (evt.name == Event.Name.mousePressed) {
+                    //Stage.textUserInfo.text(evt + emitter.toString());
+                } else if (evt.name == Event.Name.mouseReleased) {
+                    //Stage.textUserInfo.text(evt + emitter.toString());
+                } else if (evt.name == Event.Name.mouseMove) {
+                    //Stage.textUserInfo.text(evt + emitter.toString());
+                } else if (evt.name == Event.Name.pointUpdated) {
+                    //Stage.textUserInfo.text(evt + emitter.toString());
+                } else if (evt.name == Event.Name.draggableSelected) {
+                    if (emitter != null) {
+                        Stage.textUserInfo.text(evt + emitter.toString());
+                    } else {
+                        Stage.textUserInfo.text(evt + "null");
+                    }
+                } else if (evt.name == Event.Name.draggableDragged) {
+                    //Stage.textUserInfo.text(evt + emitter.toString());
+                } else if (evt.name == Event.Name.shapeModified) {
+                    //Stage.textUserInfo.text(evt + emitter.toString());
+                }
+            }
+        });
     }
 }
